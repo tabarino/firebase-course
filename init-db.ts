443ec95eb00e5ec37235bce3193764c067d6c42d
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-14 23:45:51 +0100

    5. Setting Up our Firestore NoSQL Database

--- init-db.ts
+++ init-db.ts
@@ -3,20 +3,22 @@ import {COURSES, findLessonsForCourse} from './db-data';
 
 import * as firebase from 'firebase';
 
-var config = {
-    apiKey: "AIzaSyDNPCbN20dZrAbGChRjyBSDmu2uRiPyBMk",
-    authDomain: "test-a6f61.firebaseapp.com",
-    databaseURL: "https://test-a6f61.firebaseio.com",
-    projectId: "test-a6f61",
-    storageBucket: "test-a6f61.appspot.com",
-    messagingSenderId: "24603706805"
+const config = {
+    apiKey: 'AIzaSyAaUzMZFKfmkQtGyrvYEgAQNWo3UfsftSI',
+    authDomain: 'fir-course-f8e49.firebaseapp.com',
+    databaseURL: 'https://fir-course-f8e49.firebaseio.com',
+    projectId: 'fir-course-f8e49',
+    storageBucket: 'fir-course-f8e49.appspot.com',
+    messagingSenderId: '898180964900',
+    appId: '1:898180964900:web:3dd7a5d0105d6a240baeb1',
+    measurementId: 'G-YKYF7XRQFD'
 };
 
-console.log("Uploading data to the database with the following config:\n");
+console.log('Uploading data to the database with the following config:\n');
 
 console.log(JSON.stringify(config));
 
-console.log("\n\n\n\nMake sure that this is your own database, so that you have write access to it.\n\n\n");
+console.log('\n\n\n\nMake sure that this is your own database, so that you have write access to it.\n\n\n');
 
 firebase.initializeApp(config);
 
@@ -24,7 +26,7 @@ const db = firebase.firestore();
 
 async function uploadData() {
 
-  var batch = db.batch();
+  const batch = db.batch();
 
   const courses = db.collection('courses');
 
@@ -37,7 +39,7 @@ async function uploadData() {
 
       const courseRef = await courses.add(newCourse);
 
-      const lessons = courseRef.collection("lessons");
+      const lessons = courseRef.collection('lessons');
 
       const courseLessons = findLessonsForCourse(course.id);
 
@@ -69,18 +71,18 @@ function removeId(data:any) {
 
 uploadData()
   .then(() => {
-    console.log("Writing data, exiting in 10 seconds ...\n\n");
+    console.log('Writing data, exiting in 10 seconds ...\n\n');
 
     setTimeout(() => {
 
-      console.log("\n\n\nData Upload Completed.\n\n\n");
+      console.log('\n\n\nData Upload Completed.\n\n\n');
       process.exit(0);
 
     }, 10000);
 
   })
   .catch(err => {
-    console.log("Data upload failed, reason:", err, '\n\n\n');
+    console.log('Data upload failed, reason:', err, '\n\n\n');
     process.exit(-1);
   });
 
