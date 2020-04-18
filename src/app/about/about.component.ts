a17dd2f96cbb5995ef56493f71ea5943dbfa1a93
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-18 18:34:45 +0100

    12. Querying a Firestore Collection using AngularFire

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    constructor(private db: AngularFirestore) { }
 
-  ngOnInit() {
-
-  }
+    ngOnInit() {
+        this.db.collection('courses').valueChanges().subscribe(val => {
+            console.log(val);
+        });
+    }
 
 }
