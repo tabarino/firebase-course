76d0d2a01fd49957a4aedcb80b299ef3a12f58a8
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-19 11:26:18 +0100

    18. Understanding Firestore Querying Constraints and Invalid Query Error Messages

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -13,18 +13,27 @@ export class CoursesService {
 
     loadAllCourses() {
         return this.db.collection(
-            // 'courses', ref => ref.orderBy('seqNo')
+            'courses', ref => ref.orderBy('seqNo')
             // 'courses', ref => ref.where('seqNo', '==', 0)
             // 'courses', ref => ref.where('seqNo', '>', 0).where('seqNo', '<=', 5)
             // 'courses', ref => ref.orderBy('seqNo').startAfter(0).endAt(5)
             // 'courses', ref => ref.where('categories', 'array-contains', 'BEGINNER')
+
+            // FirebaseError: "Invalid query. All where filters with an inequality (<, <=, >, or >=) must be on the same field.
+            // But you have inequality filters on 'seqNo' and 'lessonsCount'
+            // Firestore doesn't allow this type of queries because of performance issues
+            // 'courses', ref => ref.where('seqNo', '>=', 5).where('lessonsCount', '>', 5)
 
             // Firebase create an index for each field in the collection
             // In this case when you need to search by 2 or more different fields
             // Firebase will retrieve an error and you have to go and create a composite index
             // to perform the query.
             // Every query that runs in Firestore needs to have an index!!!
-            'courses', ref => ref.where('seqNo', '==', 5).where('lessonsCount', '>', 5)
+            // 'courses', ref => ref.where('seqNo', '==', 5).where('lessonsCount', '>', 5)
+
+            // In this case we also have to create a new index, because the equality is in the lessonsCount now
+            // For both cases Firestore add an error in the console with the link to create the index
+            // 'courses', ref => ref.where('seqNo', '>=', 5).where('lessonsCount', '==', 8)
         ).snapshotChanges().pipe(
             map(snaps => {
                 return snaps.map(snap => {
