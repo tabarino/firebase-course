918401b45271f5c35a0c8ac6ecf37ed3a24a887d
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-19 10:06:05 +0100

    16. Firestore Queries and Performance Guarantees - orderBy and array-contains

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -12,7 +12,13 @@ export class CoursesService {
     ) { }
 
     loadAllCourses() {
-        return this.db.collection('courses').snapshotChanges().pipe(
+        return this.db.collection(
+            'courses', ref => ref.orderBy('seqNo')
+            // 'courses', ref => ref.where('seqNo', '==', 0)
+            // 'courses', ref => ref.where('seqNo', '>', 0).where('seqNo', '<=', 5)
+            // 'courses', ref => ref.orderBy('seqNo').startAfter(0).endAt(5)
+            // 'courses', ref => ref.where('categories', 'array-contains', 'BEGINNER')
+        ).snapshotChanges().pipe(
             map(snaps => {
                 return snaps.map(snap => {
                     return <Course> {
