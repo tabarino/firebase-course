47a99a8c6c12ea436a8b676f509ddaffcfa8917f
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-19 11:00:09 +0100

    17. Firestore Compound Queries - Understanding Firestore Indexes

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -13,11 +13,18 @@ export class CoursesService {
 
     loadAllCourses() {
         return this.db.collection(
-            'courses', ref => ref.orderBy('seqNo')
+            // 'courses', ref => ref.orderBy('seqNo')
             // 'courses', ref => ref.where('seqNo', '==', 0)
             // 'courses', ref => ref.where('seqNo', '>', 0).where('seqNo', '<=', 5)
             // 'courses', ref => ref.orderBy('seqNo').startAfter(0).endAt(5)
             // 'courses', ref => ref.where('categories', 'array-contains', 'BEGINNER')
+
+            // Firebase create an index for each field in the collection
+            // In this case when you need to search by 2 or more different fields
+            // Firebase will retrieve an error and you have to go and create a composite index
+            // to perform the query.
+            // Every query that runs in Firestore needs to have an index!!!
+            'courses', ref => ref.where('seqNo', '==', 5).where('lessonsCount', '>', 5)
         ).snapshotChanges().pipe(
             map(snaps => {
                 return snaps.map(snap => {
