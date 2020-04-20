c9aa4deebc8a124b0f56e188e9ff148dacc5ca7c
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-20 21:56:20 +0100

    27. Firestore Transactions - Transactions vs Batched Writes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -41,6 +41,7 @@ export class AboutComponent implements OnInit {
         // });
 
         // Firestore Batched Writes
+        // Use it if you are going to write data in more than one doc in the database
         // There is a limit of writes using the batch - 500 operations at the same time
         // If you have more than that you have to split the batches
         // const firebaseCourseRef = this.db.doc('/courses/IkO009NXSs0USMiH8L6e').ref;
@@ -50,5 +51,25 @@ export class AboutComponent implements OnInit {
         // batch.update(firebaseCourseRef, { titles: { description: 'Serverless 123 Angular with Firebase Course' } });
         // batch.update(ngrxCourseRef, { titles: { description: 'NgRx 123 In Depth' } });
         // const batch$ = of(batch.commit());
+
+        // Run Transaction
+        // Use it if you are going to read and write data in one or more than one doc in the database
+        // this.runTransaction();
     }
+
+    // async runTransaction() {
+    //     const newCounter = await this.db.firestore.runTransaction(async transaction => {
+    //         console.log('Run Transaction ...');
+
+    //         const courseRef = this.db.doc('/courses/IkO009NXSs0USMiH8L6e').ref;
+    //         const snap = await transaction.get(courseRef);
+    //         const course = <Course> snap.data();
+    //         const lessonsCount = course.lessonsCount + 1;
+    //         transaction.update(courseRef, { lessonsCount });
+
+    //         return lessonsCount;
+    //     });
+
+    //     console.log('New Lessons Count: ' + newCounter);
+    // }
 }
