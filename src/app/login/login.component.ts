a9cfc987e71db35d7722936b12aeef0aada732e8
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-21 23:29:31 +0100

    30. Firebase UI in Action - Social Login plus Email and Password Authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebaseui from 'firebaseui';
 import * as firebase from 'firebase/app';
-
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    ui: firebaseui.auth.AuthUI;
 
-  constructor() { }
+    constructor(
+        private angularFireAuth: AngularFireAuth
+    ) { }
 
-  ngOnInit() {
-  }
+     ngOnInit() {
+        const uiConfig = {
+            signInOptions: [
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                firebase.auth.EmailAuthProvider.PROVIDER_ID
+            ],
+            callbacks: {
+                signInSuccessWithAuthResult: this.onLoginSucessful.bind(this)
+            }
+        };
 
+        this.ui = new firebaseui.auth.AuthUI(this.angularFireAuth.auth);
+        this.ui.start('#firebaseui-auth-container', uiConfig);
+    }
+
+    onLoginSucessful() {
+    }
 }
