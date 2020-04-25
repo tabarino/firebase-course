d60a69d3bc1cab15f9f8df7c4d9c1bfbc7bd07ab
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-25 09:27:09 +0100

    45. Firebase Hosting - Benefits and Quick Deployment Guide

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyAaUzMZFKfmkQtGyrvYEgAQNWo3UfsftSI',
+        authDomain: 'fir-course-f8e49.firebaseapp.com',
+        databaseURL: 'https://fir-course-f8e49.firebaseio.com',
+        projectId: 'fir-course-f8e49',
+        storageBucket: 'fir-course-f8e49.appspot.com',
+        messagingSenderId: '898180964900',
+        appId: '1:898180964900:web:3dd7a5d0105d6a240baeb1',
+        measurementId: 'G-YKYF7XRQFD'
+    }
 };
