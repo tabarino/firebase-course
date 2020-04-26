24b84ec06b2663a67be0fb0831d3d56e5a1259f2
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-26 20:15:09 +0100

    54. Implementing a Firestore Database Trigger with Firebase Cloud Functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
-import { Course } from '../../src/app/model/course'
 import { db } from './init';
 
 const app = express();
@@ -10,10 +9,11 @@ app.use(cors({ origin: true }));
 
 app.get('/courses', async (request, response) => {
     const snaps = await db.collection('courses').get();
-    const courses: Course[] = [];
+    const courses: any[] = [];
     snaps.forEach(snap => courses.push(snap.data()));
 
     response.status(200).json({ courses });
 });
 
 export const getCourses = functions.https.onRequest(app);
+export { onAddLesson } from './lessons-counter';
---
+++ functions/src/lessons-counter.ts
@@ -0,0 +1,22 @@
+import * as functions from 'firebase-functions';
+import { db } from './init';
+
+export const onAddLesson = functions.firestore.document(
+    'courses/{courseId}/lessons/{lessonId}'
+).onCreate(
+    async (snap, context) => {
+        // You can get the fields in this way
+        // const courseId = context.params.courseId;
+
+        console.log('Running onAddLesson trigger ...');
+
+        return db.runTransaction(async transaction => {
+            const courseRef = snap.ref.parent.parent;
+            const courseSnap = await transaction.get(courseRef);
+            const course = courseSnap.data();
+            const changes = { lessonsCount: course.lessonsCount + 1 };
+
+            transaction.update(courseRef, changes);
+        });
+    }
+);
