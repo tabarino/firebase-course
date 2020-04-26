3a53eb7140092fbb566e549a226b22b29e2e9b50
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-26 20:40:35 +0100

    55. Firebase Cloud Functions - Automatic Document Counter Implementation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,4 +16,4 @@ app.get('/courses', async (request, response) => {
 });
 
 export const getCourses = functions.https.onRequest(app);
-export { onAddLesson } from './lessons-counter';
+export { onAddLesson, onDeleteLesson } from './lessons-counter';
--- functions/src/lessons-counter.ts
+++ functions/src/lessons-counter.ts
@@ -10,13 +10,31 @@ export const onAddLesson = functions.firestore.document(
 
         console.log('Running onAddLesson trigger ...');
 
-        return db.runTransaction(async transaction => {
-            const courseRef = snap.ref.parent.parent;
-            const courseSnap = await transaction.get(courseRef);
-            const course = courseSnap.data();
-            const changes = { lessonsCount: course.lessonsCount + 1 };
-
-            transaction.update(courseRef, changes);
+        courseTransaction(snap, course => {
+            return { lessonsCount: course.lessonsCount + 1 };
         });
     }
 );
+
+export const onDeleteLesson = functions.firestore.document(
+    'courses/{courseId}/lessons/{lessonId}'
+).onDelete(
+    async (snap, context) => {
+        console.log('Running onDeleteLesson trigger ...');
+
+        courseTransaction(snap, course => {
+            return { lessonsCount: course.lessonsCount - 1 };
+        });
+    }
+);
+
+function courseTransaction(snap, callback: Function) {
+    return db.runTransaction(async transaction => {
+        const courseRef = snap.ref.parent.parent;
+        const courseSnap = await transaction.get(courseRef);
+        const course = courseSnap.data();
+        const changes = callback(course);
+
+        transaction.update(courseRef, changes);
+    });
+}
