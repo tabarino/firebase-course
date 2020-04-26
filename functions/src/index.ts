ae165bdabb484cf91edf385bae3a99ca9a0bedcc
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-26 17:04:08 +0100

    52. Firebase Cloud Function REST Endpoint with Express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,19 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
+import { db } from './init';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const cors = require('cors');
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    response.status(200).json({ message: 'Hello World' });
+const app = express();
+
+app.use(cors({ origin: true }));
+
+app.get('/courses', async (request, response) => {
+    const snaps = await db.collection('courses').get();
+    const courses: any[] = [];
+    snaps.forEach((snap: { data: () => any; }) => courses.push(snap.data()));
+
+    response.status(200).json({ courses });
 });
+
+export const getCourses = functions.https.onRequest(app);
