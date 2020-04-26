ca3e471bf69a7e2a8311cbedf1a58373dbe03ad6
tabarino <tabarino@outlook.com>
2020-04-26 22:50:32 +0100

    57. Download a file from Firebase Storage from inside a Firebase Cloud Function

--- functions/src/image-upload.ts
+++ functions/src/image-upload.ts
@@ -1,6 +1,11 @@
 import * as functions from 'firebase-functions';
 
 const path = require('path');
+const os = require('os');
+const mkdirp = require('mkdirp-promise')
+const { Storage } = require('@google-cloud/storage');
+
+const gcs = new Storage();
 
 export const resizeThumbnail = functions.storage.object().onFinalize(
     async (object, context) => {
@@ -8,12 +13,24 @@ export const resizeThumbnail = functions.storage.object().onFinalize(
         const contentType = object.contentType || '';
         const fileDir = path.dirname(fileFullPath);
         const fileName = path.basename(fileFullPath);
+        const tempLocalDir = path.join(os.tmpdir(), fileDir);
 
         console.log('Thumbnail generation started: ', fileFullPath, fileDir, fileName);
 
         if (contentType.startsWith('image/')) {
+            console.log('Exiting image processing.');
             return null;
         }
+
+        await mkdirp(tempLocalDir);
+
+        const bucket = gcs.bucket(object.bucket);
+        const originalImageFile = bucket.file(fileFullPath);
+        const tempLocalFile = path.join(os.tmpdir(), fileFullPath);
+
+        console.log('Downloading image to: ', tempLocalFile);
+
+        await originalImageFile.download({ destination: tempLocalFile });
 
         return null;
     }
