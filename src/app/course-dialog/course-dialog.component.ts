8e836e4124c1fd8e174e5391e7d6484b325940c5
Ivan Tabarino <itabarino@Ivans-MBP.lan>
2020-04-26 10:15:45 +0100

    48. Implementing a File Upload Percentage Indicator with Angular Material

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -4,6 +4,7 @@ import { Course } from '../model/course';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { CoursesService } from '../services/courses.service';
 import { AngularFireStorage } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'course-dialog',
@@ -14,6 +15,7 @@ export class CourseDialogComponent implements OnInit {
     form: FormGroup;
     description: string;
     course: Course;
+    uploadPercent$: Observable<number>;
 
     constructor(
         private fb: FormBuilder,
@@ -52,6 +54,7 @@ export class CourseDialogComponent implements OnInit {
         const filePath = `courses/${this.course.id}/${file.name}`;
         const task = this.storage.upload(filePath, file);
 
+        this.uploadPercent$ = task.percentageChanges();
         task.snapshotChanges().subscribe(console.log);
     }
 }
